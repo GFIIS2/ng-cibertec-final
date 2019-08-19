c009ce7ab0babf180eaf011770faf9426a5804df
Gustavo Yauri <gryauri@gmail.com>
2019-08-19 01:42:19 -0500

    Actualizacion del boton eliminar y estilos

--- src/app/views/users/list-user/list-user.component.ts
+++ src/app/views/users/list-user/list-user.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import User from '../user.model';
 import { UsersService } from '../shared/services/users.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-user',
@@ -9,20 +11,30 @@ import { UsersService } from '../shared/services/users.service';
 })
 export class ListUserComponent implements OnInit {
 
+  deleteSubs: Subscription;
   allUsers: User[];
 
   constructor(
+    private router: Router,
     private userService: UsersService
   ) {  }
 
   ngOnInit() {
-    this.userService.getUsers().subscribe((users: User[]) => {
+    this.userService.getUsers()
+    .subscribe((users: User[]) => {
       this.allUsers = users;
     })
   }
 
   onRemove(id: number) {    
-    this.userService.deleteUser(id);
-    this.allUsers = this.allUsers.filter(usr =>  usr.id !== id);
+    
+    this.deleteSubs = this.userService.deleteUser(id).subscribe();  
+    this.allUsers = this.allUsers.filter(usr =>  usr.id !== id);   
+  }
+  
+  ngOnDestroy(){
+    if(this.deleteSubs){
+      this.deleteSubs.unsubscribe();
+    }
   }
 }
